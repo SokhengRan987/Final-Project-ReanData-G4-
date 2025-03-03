00007e80b32cdee7a76434237afeeb40e113073a
Panharoth06 <panharothchheng0903@gmail.com>
2025-03-03 16:38:31 +0700

    Update: updated nav bar ui

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -83,7 +83,7 @@ export default function NavbarComponent() {
         </div>
 
         {/* Navigation Links */}
-        <div
+        {/* <div
           className={`w-full md:flex md:w-auto md:order-1 ${
             dropdownOpen ? "block" : "hidden"
           }`}
@@ -102,6 +102,48 @@ export default function NavbarComponent() {
                 </li>
               )
             )}
+          </ul>
+        </div> */}
+        <div
+          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+            dropdownOpen ? "block" : "hidden"
+          }`}
+          id="navbar-cta"
+        >
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded-sm md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                aria-current="page"
+              >
+                Datasets
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded-sm  md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Documentation
+              </a>
+            </li>
+            <li>
+              <a
+                href="/about-us"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded-sm md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded-sm md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Help & Support
+              </a>
+            </li>
           </ul>
         </div>
       </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,21 +5,21 @@ import NavbarComponent from "../components/NavbarComponent";
 import FooterV2 from "../components/FooterSampleV2";
 import FooterSampleV2 from "../components/FooterSampleV2";
 export default function About() {
-    useEffect(() => {
-      const handleWheel = (event) => {
-        if (event.ctrlKey) return; // Allow zooming with Ctrl + Scroll
+    // useEffect(() => {
+    //   const handleWheel = (event) => {
+    //     if (event.ctrlKey) return; // Allow zooming with Ctrl + Scroll
     
-        event.preventDefault();
-        window.scrollBy({
-          top: event.deltaY > 0 ? 250 : -250, // Faster scrolling
-          behavior: "smooth",
-        });
-      };
+    //     event.preventDefault();
+    //     window.scrollBy({
+    //       top: event.deltaY > 0 ? 250 : -250, // Faster scrolling
+    //       behavior: "smooth",
+    //     });
+    //   };
     
-      window.addEventListener("wheel", handleWheel, { passive: false });
+    //   window.addEventListener("wheel", handleWheel, { passive: false });
     
-      return () => window.removeEventListener("wheel", handleWheel);
-    }, []);
+    //   return () => window.removeEventListener("wheel", handleWheel);
+    // }, []);
   return (
     <>
       <NavbarComponent />
