efa516bb081b7f78a7e3430e5009b00f429d382e
Panharoth06 <panharothchheng0903@gmail.com>
2025-03-15 10:24:51 +0700

    89%

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [react()],
-
-
   // server: {
   //   proxy: {
   //     '/rpc': {
